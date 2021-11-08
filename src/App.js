aa6da04580151ad048efeae1298ecbdc5558e63f
Paula Carril Torrens <PAULAANDREACARRIL@GMAIL.COM>
2021-11-08 20:08:22 +1100

    set up vs code react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>Paula's Dictionary</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
