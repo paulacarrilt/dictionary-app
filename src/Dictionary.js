34cee931ea8a217cda4c8bbd2bf537f6f63d3668
Paula Carril Torrens <PAULAANDREACARRIL@GMAIL.COM>
2021-11-09 16:13:26 +1100

    fixed api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -11,7 +11,6 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
-
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
     axios.get(apiUrl).then(handleResponse);
   }
