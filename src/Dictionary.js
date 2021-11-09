0a528c2131b7f5ed5c62fe2b19bb92f59fc593da
Paula Carril Torrens <PAULAANDREACARRIL@GMAIL.COM>
2021-11-09 16:09:43 +1100

    added axios and API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,21 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   const [word, setWord] = useState(null);
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${word}`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    axios.get(apiUrl).then(handleResponse);
   }
+
   function handleWord(event) {
     setWord(event.target.value);
   }
