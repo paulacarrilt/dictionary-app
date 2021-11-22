7b9eafdae084011c21af9c2a6601215739342bdc
Paula Carril Torrens <PAULAANDREACARRIL@GMAIL.COM>
2021-11-22 17:01:27 +1100

    added alt text to img

--- src/Photos.js
+++ src/Photos.js
@@ -11,7 +11,7 @@ export default function Photos(props) {
             return (
               <div className="col-4" key={index}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
-                  <img src={photo.src.tiny} />
+                  <img src={photo.src.tiny} alt={photo.src.photographer} />
                 </a>
               </div>
             );
